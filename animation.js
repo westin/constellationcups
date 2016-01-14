c465398b6756662475687280e2bc95b2a755f2d6
Westin Lohne <westin@cmu.edu>
2016-01-13 23:57:06 -0500

    draw waypoints (but not the ones too close :D)

--- animation.js
+++ animation.js
@@ -1,34 +1,16 @@
-// some initial star stuff
+function downloadDataUri(options) {
+	if (!options.url)
+		options.url = "http://download-data-uri.appspot.com/";
+	$('<form method="post" action="' + options.url
+		+ '" style="display:none"><input type="hidden" name="filename" value="'
+		+ options.filename + '"/><input type="hidden" name="data" value="'
+		+ options.data + '"/></form>').appendTo('body').submit().remove();
+}
 
-
-// var starPath1 = "M262.825007,167 L264.540905,168.36331 L266.278803,167.113278 L265.258953,169.026826 L267.017681,170.369305 L264.97927,170.097463 L264.812747,171.927808 L264.021794,169.889397 L262,169.845163 L263.561077,168.830345 L262.825007,167 Z"
-// var starPath2 = "M249.13078,166 L251.080893,168.870331 L253.836463,167.733216 L251.98488,170.002364 L253.767115,172.276759 L251.206474,171.274241 L250.45184,173.864064 L249.974271,170.847987 L247,170.889957 L249.865085,169.402821 L249.13078,166 Z"
-//
-//
-// var star1 = new Path(starPath1)
-// star1.fillColor = 'black'
-// star1.scale(3)
-// star1.position = view.center;
-//
-//
-// var star2 = new Path(starPath2)
-// star2.fillColor = 'black'
-// star2.scale(2.5)
-// star2.position = view.center + 80;
-//
-// var star1Clone = star2.clone()
-// star1Clone.rotate(45)
-// star1Clone.position = [view.center.x - 15, view.center.y - 100];
-//
-// var line1 = new Path();
-// line1.strokeColor = 'black';
-// line1.strokeWidth = 1
-// line1.dashArray = [2, 5];
-// line1.add(star1Clone.position, star1.position, star2.position);
-
-// paper.install(window);
-// paper.setup('mainCanvas');
-//
-// function makeLines(directionSteps){
-//   console.log(directionSteps)
-// }
+function onMouseDown(event) {
+	var svg = project.exportSVG({ asString: true });
+	downloadDataUri({
+		data: 'data:image/svg+xml;base64,' + btoa(svg),
+		filename: 'export.svg'
+	});
+}
